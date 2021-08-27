662daeb6f93874a925501c439b6dab80b503dd1f
Fergus-Molloy <fergus@molloy.xyz>
2021-08-27 10:21:37 +0100

    Initial bare metal binary

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
